08823203382504bf31263f1a1f93cf8b6bf067f1
Alvin Wilta <51949757+alvinwilta@users.noreply.github.com>
2021-02-10 12:18:01 +0700

    Latihan kuis 2016

---
+++ latihan2/FooMem.hpp
@@ -0,0 +1,45 @@
+//file FooMem.hpp
+
+#ifndef _FOOMEM_HPP_
+#define _FOOMEM_HPP_
+
+#include <iostream>
+
+class FooMem{
+    private:
+    int * Mem;
+    int Size;
+    int Left;
+    int Right;
+    const int Max;
+
+    public:
+    //ctor
+    FooMem();
+    //ctor objek bertype FooMem, memori dialokasikan sebesar _s
+    //size=_s<=Max
+    FooMem(int _s);
+    //cctor
+    FooMem(FooMem& F);
+    //dtor
+    ~FooMem();
+    //operator
+    FooMem& operator=(FooMem& F);
+
+    //Fungsi
+    int GetLeft();
+    //Mengembalikan Posisi Mem yang terisi terakhir di kiri
+    int GetRight();
+    //Mengembalikan Posisi Mem yang terisi terakhir di kanan
+
+    //Predikat Kosong
+    bool IsEmpty() const;
+    //Predikat Penuh
+    bool IsFull() const;
+
+    //Add
+    void Add(int x); //Menambahkan x ke Mem dengan aturan yang dijelaskan
+    //Del
+    int Del(); //Menghapus elemen FooMem sesuai aturan
+};
+#endif
